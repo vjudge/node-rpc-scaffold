851155336b836f05476c0f39c0e97ccb47255bf1
vjudge <luodan.guo@eeoa.com>
2024-05-10 09:38:38 +0000

    feat: add client

--- src/client.ts
+++ src/client.ts
@@ -1,20 +1,16 @@
 import config from 'config';
 import { credentials } from '@grpc/grpc-js';
 
-import { getLogger, log4js } from '@camin/server-common';
 import { GreeterClient, HelloRequest, HelloResponse } from './models/helloworld';
-
-log4js.configure(config.get('log4js'));
-const logger = getLogger(__dirname);
 
 async function main() {
   const client: GreeterClient = new GreeterClient(`127.0.0.1:${config.get('port')}`, credentials.createInsecure());
 
   client.sayHello({ name: 'vjudge!' }, (err, response) => {
     if (err) {
-      logger.error('error: ', { err });
+      console.error('error: ', { err });
     }
-    logger.log('response: ', response.message);
+    console.log('response: ', response.message);
   })
   
 }
